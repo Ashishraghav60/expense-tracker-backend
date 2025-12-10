3b7acc982b32dce2d9f728702d0eb835dddfd435
Ash <ashishraghav60@gmail.com>
2025-12-10 14:20:48 +0530

    Updated app.py with home route and final backend fixes

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
 from config import Config
-from database.models import db
+from database.db import db
 from routes.auth_routes import auth_bp
 from routes.expense_routes import expense_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 CORS(app)
 
-# Initialize DB
 db.init_app(app)
 
-with app.app_context():
-    db.create_all()
-
-# Register Blueprints
+# Register blueprints
 app.register_blueprint(auth_bp, url_prefix="/api")
 app.register_blueprint(expense_bp, url_prefix="/api")
 
+@app.route("/")
+def home():
+    return {"message": "Expense Tracker Backend is running!"}
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=5000)
